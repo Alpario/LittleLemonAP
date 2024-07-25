b51fd3b5cb293f62c581d61b641d71cdae46b6bc
Aldo Pariota <aldopariota@MBP-di-Aldo.wind3.hub>
2024-07-25 22:52:43 +0200

    semantic html

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return(
+    <header>
+      <h1>Welcome to My Website</h1>
+      <p>This is the header section.</p>
+    </header>
+  );
+}
+
+export default Header;
